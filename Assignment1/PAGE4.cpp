852531087c59768439cc4d1bbe3bef9494758223
boonyakorn <boonyakornboontang@gmail.com>
2024-07-26 19:23:32 +0700

    Create PAGE4.cpp

---
+++ Assignment1/PAGE4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 3 ;
+    int b = 4 ;
+
+    if( a > b ) {
+        printf( "The number in A is greater than B.\n" ) ;
+    }//end if
+
+    printf( "\n Hellow World." ) ;
+
+    return 0 ;
+
+}//end main function
